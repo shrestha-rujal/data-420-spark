d54ec0e85763ad0dd5a17e651540de69b6c5f5f2
Rujal Shrestha <rujal4shrz@gmail.com>
2025-10-07 16:22:42 +1300

    complete question 3 audio similarity

--- Assignment 2/.ipynb_checkpoints/helpers-checkpoint.py
+++ Assignment 2/.ipynb_checkpoints/helpers-checkpoint.py
@@ -1,10 +1,14 @@
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
+from pyspark.sql.window import Window
 import getpass
 import pandas
 import pyspark
 import random
 import re
+from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
+
+# WIP - docstrings will be added at the end if sufficient time is remaining for a final refactoring
 
 # better to use environment variables for token keys, and directory paths, but hardcoded for the purposes
 # of this assignment, will be refactored later if sufficient time is left for refactoring
@@ -49,3 +53,69 @@ def load_feature(spark, feature_name):
     renamed_features = features.toDF(*new_names)
 
     return renamed_features
+
+# helper function to train test split in a stratified manner
+def stratified_split(df, label_col, train_fraction=0.8, seed=42):
+    
+    window_spec = Window.partitionBy(label_col).orderBy(F.rand(seed))    
+    df_ranked = df.withColumn("row_number", F.row_number().over(window_spec))
+    class_counts = df_ranked.groupBy(label_col).agg(F.count("*").alias("n")).collect()
+    cutoffs = {row[label_col]: int(row["n"] * train_fraction) for row in class_counts}
+
+    # combine all conditions to help label train rows
+    condition = None
+    for cls, cutoff in cutoffs.items():
+        cond = ((F.col(label_col) == cls) & (F.col("row_number") <= cutoff))
+        condition = cond if condition is None else (condition | cond)
+
+    # Add is_train flag
+    df_flagged = df_ranked.withColumn("is_train", F.when(condition, True).otherwise(False))
+
+    # Split
+    train_df = df_flagged.filter(F.col("is_train") == True).drop("row_number", "is_train")
+    test_df = df_flagged.filter(F.col("is_train") == False).drop("row_number", "is_train")
+
+    return train_df, test_df
+
+# function to calculate class weights using Inverse Frequency Weights
+# returns the df with added weight column
+def apply_class_weights(df, label_col="label", weight_col="weight"):    
+    class_counts = df.groupBy(label_col).count().collect()
+    total = df.count()
+    num_classes = len(class_counts)
+
+    # inverse frequency weights formula
+    weights = {row[label_col]: total / (num_classes * row["count"]) for row in class_counts}
+
+    # **complex line**
+    # flatten the weight dictionary to be used as literals in create_map suitable for use with spark dataframe
+    weight_map = F.create_map([F.lit(x) for kv in weights.items() for x in kv])
+
+
+    df_weighted = df.withColumn(weight_col, weight_map[F.col(label_col)])
+
+    return df_weighted
+
+# calculate evaluation metrics for multiclass predictions
+def get_multiclass_metrics(predictions, label_col="label"):
+    evaluator = MulticlassClassificationEvaluator(labelCol=label_col, predictionCol="prediction")
+
+    accuracy = evaluator.setMetricName("accuracy").evaluate(predictions)
+    f1 = evaluator.setMetricName("f1").evaluate(predictions)
+    precision = evaluator.setMetricName("weightedPrecision").evaluate(predictions)
+    recall = evaluator.setMetricName("weightedRecall").evaluate(predictions)
+
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1": f1
+    }
+
+
+
+
+
+
+
+
